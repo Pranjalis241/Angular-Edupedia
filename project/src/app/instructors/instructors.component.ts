e021826162791d5aa6c696bb3bf55e289264f6e2
Pranjali Singh <pranjali.s@impelsys.com>
2025-02-05 10:48:48 +0530

    image

--- project/src/app/instructors/instructors.component.ts
+++ project/src/app/instructors/instructors.component.ts
@@ -14,37 +14,37 @@ export class InstructorsComponent {
       name: 'John Carter',
       role: 'Data Science ',
       description: 'An experienced data scientist with Ph.D. in Computer Science',
-      image: '/image/man.jpg'
+      image: 'assets/image/man.jpg'
     },
     {
       name: 'Sophie Moore',
       role: 'DSA Specialist',
       description: 'Ut et turpis et pellentesque, sollicitudin malesuada.',
-      image: '/image/rahul_.jpg'
+      image: 'assets/image/rahul_.jpg'
     },
     {
       name: 'Norma Hicks',
       role: 'iOS Developer',
       description: 'Ut et turpis et pellentesque, sollicitudin malesuada.',
-      image: '/image/shreya-removebg-preview.jpg'
+      image: 'assets/image/shreya-removebg-preview.jpg'
     },
     {
       name: 'Kellie Burns',
       role: 'SEO Specialist',
       description: 'Ut et turpis et pellentesque, sollicitudin malesuada.',
-      image: '/image/naman-removebg-preview.jpg'
+      image: 'assets/image/naman-removebg-preview.jpg'
     },
     {
       name: 'Alex Paul',
       role: 'Brand Designer',
       description: 'Ut et turpis et pellentesque, sollicitudin malesuada.',
-      image: '/image/teacher.jpg'
+      image: 'assets/image/teacher.jpg'
     },
     {
       name: 'Fred Owen',
       role: 'Entrepreneur',
       description: 'Ut et turpis et pellentesque, sollicitudin malesuada.',
-      image: '/image/sir-removebg-preview.jpg'
+      image: 'assets/image/sir-removebg-preview.jpg'
     }
   ];
 }
